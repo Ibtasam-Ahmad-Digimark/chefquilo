ce05939844a22429a30fd1b382bb8b1bf69d80db
Ibtasam-Ahmad-Digimark <ibtasam_ahmad@digimarkdevelopers.com>
2024-09-18 16:08:21 +0500

    Updated Prompt

--- agent.py
+++ agent.py
@@ -1,16 +1,22 @@
 import streamlit as st
 from openai import OpenAI
+from dotenv import load_dotenv
 import os
-
+load_dotenv()
 
 client = OpenAI(api_key=st.secrets["OPEN_AI_API_KEY"])
 
+
 def generate_response(message_history):
     messages = [{"role": m["role"], "content": m["content"]} for m in message_history]
+
     messages.insert(0, {
-        "role": "system", 
-        "content": """You are a helpful and experienced chef named Chef-GPT, and your goal is to guide users through cooking processes as if they are beginners. 
-        After explaining each step in detail, ask if they understand or have any questions. Only proceed to the next step if they confirm their understanding."""})
+    "role": "system", 
+    "content": """You are a helpful and experienced chef named Chef-GPT. Your sole purpose is to guide users through cooking processes step by step, as if they are beginners, and answer questions only related to cooking.
+    Do not respond to or entertain any queries that are not related to cooking.
+    First, provide the list of ingredients. Then, give one step of the process at a time. After explaining each step in detail, ask if they understand or have any questions related to the cooking process. Only proceed to the next step if they confirm their understanding."""
+    })
+
     response = client.chat.completions.create(
                 model="gpt-3.5-turbo-0125",
                 messages=messages
@@ -19,11 +25,14 @@ def generate_response(message_history):
 
 def generate_stream(message_history):
     messages = [{"role": m["role"], "content": m["content"]} for m in message_history]
+    
     messages.insert(0, {
-        "role": "system", 
-        "content": """You are a helpful and experienced chef named Chef-GPT, and your goal is to guide users through cooking processes step by step, as if they are beginners. 
-        After explaining each step, ask if they understand or have any questions. Only proceed to the next step if they confirm their understanding. 
-        Make sure to break complex instructions into smaller steps."""})
+    "role": "system", 
+    "content": """You are a helpful and experienced chef named Chef-GPT. Your sole purpose is to guide users through cooking processes step by step, as if they are beginners, and answer questions only related to cooking.
+    Do not respond to or entertain any queries that are not related to cooking.
+    First, provide the list of ingredients. Then, give one step of the process at a time. After explaining each step in detail, ask if they understand or have any questions related to the cooking process. Only proceed to the next step if they confirm their understanding."""
+    })
+
     stream = client.chat.completions.create(
                 model="gpt-3.5-turbo-0125",
                 messages=messages,
